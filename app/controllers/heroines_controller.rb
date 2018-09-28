49cbe92a94d27456fa67c816a95717317c1c01c4
Trevor Jameson <github email address>
2018-09-28 11:37:07 -0400

    Done.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,40 @@
 class HeroinesController < ApplicationController
+
+  before_action :find_heroine, only: [:show, :edit, :update]
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(name: params["heroine"][:name], super_name: params["heroine"][:super_name], power_id: params["heroine"][:power_id])
+    render :show
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+
+private
+
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  # def strong_params
+  #   params.require(:heroine).permit(:id, :name, :super_name, :power_id)
+  # end
 end
