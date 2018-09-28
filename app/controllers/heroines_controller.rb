e75897bb89367ba2538ea4f229a53d04c3c8be6a
github username <github email address>
2018-09-28 10:49:23 -0400

    Second code challene. Submitting before attempting bonus.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,24 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create    
+    @heroine = Heroine.create(heroine_params)
+    redirect_to heroines_path
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
