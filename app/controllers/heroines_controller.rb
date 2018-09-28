caf0ee3d8ad064670564abdfc0e539a6133c11a7
github username <github email address>
2018-09-28 10:54:34 -0400

    Done.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,35 @@
 class HeroinesController < ApplicationController
   def index
+      # byebug
+   # if params[:power_name]
+   #    power = Power.find_by(name: params[:power_name])
+   #    if power == nil
+   #        @heroines = Heroine.all
+   #    else
+   #        @heroines = Heroine.where('power_id LIKE ?', "%#{params[:power_name]}%")
+   #    end
+   #
+   # else
     @heroines = Heroine.all
+   # end
+
   end
+
+  def show
+      @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+      @heroine = Heroine.new
+  end
+
+  def create
+      @heroine = Heroine.create(params.require(:heroine).permit(:name, :super_name, :power_id))
+      if @heroine.save
+          redirect_to @heroine
+      else
+          render "new"
+      end
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,6 @@
 class Heroine < ApplicationRecord
+    #all speedsters have the same power etc.
+    belongs_to :power
+
+    validates :super_name, uniqueness: true
 end
