e98dd3d84b3500c6b5c4ba48d4c3e9f49bb72da1
patti-c <patricia.carter@flatironschool.com>
2018-09-28 11:38:44 -0400

    My challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,57 @@
 class HeroinesController < ApplicationController
+
+  before_action(:find_heroine, {only: [:show, :edit, :update]})
+
   def index
-    @heroines = Heroine.all
+    if !params[:commit].nil?
+      @heroines = Heroine.find_by_power(params[:powers])
+    else
+      @heroines = Heroine.all
+    end
+    @powers = Power.all
   end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      redirect_to new_heroine_path
+    end
+  end
+
+  def search
+    @power = Power.find(params[:id])
+  end
+
+  def edit
+    @powers = Power.all
+  end
+
+  def update
+    if @heroine.update(heroine_params)
+      redirect_to heroine_path(@heroine)
+    else
+      redirect_to edit_heroine_path
+    end
+  end
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id, :img_url)
+  end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,48 @@
 class PowersController < ApplicationController
+
+  before_action(:find_power, only: [:show, :edit, :update])
+
   def index
     @powers = Power.all
   end
+
+  def show
+    @power.description = @power.description.slice(0,1).capitalize + @power.description.slice(1..-1)
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    @power = Power.new(power_params)
+    if @power.save
+      redirect_to power_path(@power)
+    else
+      redirect_to new_power_path
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @power.update(power_params)
+      redirect_to power_path(@power)
+    else
+      redirect_to edit_power_path(@power)
+    end
+  end
+
+  private
+
+  def find_power
+    @power = Power.find(params[:id])
+  end
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
+
 end
